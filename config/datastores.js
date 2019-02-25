9196720eecaf10372c3d9ec378efc49c6bc884a0
Monaka22 <MrTawatchai22@outlook.com>
2019-02-25 14:21:02 +0700

    update db

--- config/datastores.js
+++ config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mysql',
-    url: 'mysql://root:1122334455ab@localhost:3306/hrdb'
+    url: 'mysql://root@localhost:3306/hrdb'
   },
 
 
