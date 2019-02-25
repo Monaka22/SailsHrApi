59c28b466b8092c713f9321f48b719c8fae7da6d
Monaka22 <MrTawatchai22@outlook.com>
2019-02-25 15:28:28 +0700

    long agian

--- config/security.js
+++ config/security.js
@@ -31,8 +31,8 @@ module.exports.security = {
   cors: {
     allRoutes: true,
     allowOrigins: '*',
-    allowCredentials: false,
-  },
+    allowCredentials: false
+  }
 
 
   /****************************************************************************
@@ -49,6 +49,6 @@ module.exports.security = {
   *                                                                           *
   ****************************************************************************/
 
-   csrf: true
+  //  csrf: true
 
 };
