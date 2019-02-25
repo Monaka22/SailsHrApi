01fbaee844fe25675704fee2a4923ba8062019d2
Monaka22 <MrTawatchai22@outlook.com>
2019-02-25 18:52:12 +0700

    Update  security

--- config/security.js
+++ config/security.js
@@ -28,15 +28,15 @@ module.exports.security = {
    *                                                                          *
    ***************************************************************************/
 
-  // cors: {
-  //   allRoutes: true,
-  //   allowOrigins: '*',
-  //   allowRequestMethods:'GET, POST, PUT, DELETE, OPTIONS, HEAD',
-  //   allowRequestHeaders : 'content-type',
-  //   allowResponseHeaders : '',
-  //   allowCredentials : false,
-  //   allowAnyOriginWithCredentialsUnsafe :false
-  // }
+  cors: {
+    allRoutes: true,
+    allowOrigins: '*',
+    allowRequestMethods:'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+    allowRequestHeaders : 'content-type',
+    allowResponseHeaders : '',
+    allowCredentials : false,
+    allowAnyOriginWithCredentialsUnsafe :false
+  }
 
 
   /****************************************************************************
